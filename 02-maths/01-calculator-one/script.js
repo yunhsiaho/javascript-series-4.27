bae9a4bdf37ce742422097902f5dec5e032b790d
Dhaibuna <nataschagritte@gmail.com>
2021-02-09 09:41:18 +0100

    add exos 2.1 - 2.3

--- 02-maths/01-calculator-one/script.js
+++ 02-maths/01-calculator-one/script.js
@@ -9,22 +9,50 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+
+
 (function() {
     // to get the value of an input: document.getElementById("element-id").value
 
+
     document.getElementById("addition").addEventListener("click", function() {
+
+        let operandOne = parseInt(document.getElementById("op-one").value);
+        let operandTwo = parseInt(document.getElementById("op-two").value);
+
+        alert(operandOne + operandTwo);
+
         // perform an addition
     });
-
     document.getElementById("substraction").addEventListener("click", function() {
+
+
+        let operandOne = parseInt(document.getElementById("op-one").value);
+        let operandTwo = parseInt(document.getElementById("op-two").value);
+
+        alert(operandOne - operandTwo);
+
         // perform an substraction
     });
 
     document.getElementById("multiplication").addEventListener("click", function() {
+
+
+        let operandOne = parseInt(document.getElementById("op-one").value);
+        let operandTwo = parseInt(document.getElementById("op-two").value);
+
+        alert(operandOne * operandTwo);
+
         // perform an multiplication
     });
 
     document.getElementById("division").addEventListener("click", function() {
+
+        let operandOne = parseInt(document.getElementById("op-one").value);
+        let operandTwo = parseInt(document.getElementById("op-two").value);
+
+        alert(operandOne / operandTwo);
+
         // perform an division
     });
 })();
