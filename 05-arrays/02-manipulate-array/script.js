50779c1387ac37a16f25db8a40246e2b6137d718
RekhaDev <rekhalambotte246@gmail.com>
2021-02-12 12:08:59 +0100

    ex 4.4 - 5.1 - 5.2

--- 05-arrays/02-manipulate-array/script.js
+++ 05-arrays/02-manipulate-array/script.js
@@ -25,5 +25,19 @@
     ];
 
     // your code here
+    document.getElementById("run").addEventListener("click", function(){
+
+        //let first= fruits.unshift();
+        //let end= fruits.push("kiwi");
+
+        //fruits[0]="banana";
+        //fruits[fruits.length-1]="kiwi";
+
+        fruits.splice(0,1,"banana");
+        fruits.splice(-1,1,"kiwi");
+        console.log(fruits)
+
+    })
+    
 
 })();
