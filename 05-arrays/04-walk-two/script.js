6361331cb7d2112137e2572f98d0b8716e996ed1
Nicolas Jamar <32648286+NicolasJamar@users.noreply.github.com>
2021-02-23 14:43:10 +0100

    Update script.js

--- 05-arrays/04-walk-two/script.js
+++ 05-arrays/04-walk-two/script.js
@@ -27,5 +27,10 @@
     ];
 
     // your code here
+    document.getElementById('run').addEventListener('click', function (){
+
+        fruits.forEach(element => console.log(element));
+
+    });
 
 })();
