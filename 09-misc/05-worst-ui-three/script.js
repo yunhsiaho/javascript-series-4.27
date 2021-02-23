6ac231550ee4a059c6920dcaa14cee626c1c7fe6
Nicolas Jamar <32648286+NicolasJamar@users.noreply.github.com>
2021-02-23 14:50:50 +0100

    Update script.js

--- 09-misc/05-worst-ui-three/script.js
+++ 09-misc/05-worst-ui-three/script.js
@@ -11,7 +11,6 @@
 
 (function() {
 
-    // your code here
     //CODE A AMELIORER
     let numbers =  [460, 0, 0, 0];
     let inputId = ['part-one', 'part-two', 'part-three', 'part-four']
@@ -53,5 +52,44 @@
             getInput(i);
         }),
     );
+    
+    
+    
+    // COMPLETE SOLUTION 
+    // const target = document.getElementById("target");
+    // const buttons = Array.from(document.querySelectorAll("button"));
+    // const inputs = Array.from(document.querySelectorAll("input"));
+
+    // const DELAY = 125;
+
+    // buttons.forEach((button, i) => {
+    //     const interval = () => {
+    //         const input = inputs[i];
+
+    //         let value = parseFloat(input.value);
+
+    //         if (++value > parseFloat(input.getAttribute("data-max"))) {
+    //             value = input.getAttribute("data-min");
+    //         }
+
+    //         input.value = `${value}`.padStart(2, "0");
+
+    //         // kinda ugly, but, hey, I'm the coach!
+    //         target.innerText = `+${inputs.map(inp => inp.value).join("")}`;
+    //     };
+
+    //     let intervalID = setInterval(interval, DELAY);
+
+    //     button.addEventListener("click", () => {
+    //         if (intervalID) {
+    //             clearInterval(intervalID);
+    //             intervalID = null;
+    //             button.innerText = "Start";
+    //         } else {
+    //             intervalID = setInterval(interval, DELAY);
+    //             button.innerText = "Stop";
+    //         }
+    //     });
+    // });
 
 })();
