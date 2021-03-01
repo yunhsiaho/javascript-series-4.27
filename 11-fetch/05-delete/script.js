3578ecfcf0657c51f0c4ba8dc796805ac47d30fd
leny <info@flatland.be>
2021-03-01 12:05:18 +0100

    Correction for serie 11

--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -10,5 +10,30 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+    const input = document.querySelector("#hero-id");
+
+    document.querySelector("#run").addEventListener("click", async () => {
+        const id = +input.value;
+
+        if (isNaN(id) || id === 0) {
+            console.error("Invalid id!");
+            return;
+        }
+
+        try {
+            const response = await fetch(`//localhost:3000/heroes/${id}`, {
+                method: "DELETE",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+            });
+
+            const deletedHero=await response.json();
+
+            console.log(deletedHero);
+        } catch (err) {
+            console.error(`Unknown hero with id: ${id}!`);
+        }
+    });
+
 })();
